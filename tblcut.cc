a48611efff1ac3153b1fc849287382b3addce1d1
Yuri D'Elia <yuri.delia@eurac.edu>
2010-05-21 16:18:08 +0200

    Also implement tblcut -n (number) selector.

--- tblcut.cc
+++ tblcut.cc
@@ -34,13 +34,14 @@ void
 help(char* argv[])
 {
   cerr << argv[0] << ": bad parameters:\n"
-       << "Usage: " << argv[0] << " [-hd] -f col,col,... file\n"
+       << "Usage: " << argv[0] << " [-hd] < -f col,col,... | -n col,col,... > file\n"
        << "tblcut allows to extract single columns by name from the selected CSV file.\n"
        << "CSV files are TAB separated, containing column labels on the first row. You\n"
        << "can change the column separator by setting the TBLSEP environment variable.\n"
        << "\n"
        << "  -d sep:		set a different column separator\n"
-       << "  -f col,col,...:	extract the selected columns (mandatory)\n"
+       << "  -f col,col,...:	extract the selected column names\n"
+       << "  -n col,col,...:	extract the selected column numbers\n"
        << "  -h:			help summary\n";
 }
 
@@ -48,11 +49,12 @@ help(char* argv[])
 int
 main(int argc, char* argv[]) try
 {
-  vector<string> fields;
+  vector<string> fieldNames;
+  vector<size_t> fieldNums;
   char sep = 0;
 
   int arg;
-  while((arg = getopt(argc, argv, "hf:d:")) != -1)
+  while((arg = getopt(argc, argv, "hf:n:d:")) != -1)
     switch(arg)
     {
     case 'h':
@@ -64,7 +66,16 @@ main(int argc, char* argv[]) try
       break;
 
     case 'f':
-      tokenize(fields, optarg, ",", true);
+      tokenize(fieldNames, optarg, ",", true);
+      break;
+
+    case 'n':
+      {
+	vector<string> tmp;
+	tokenize(tmp, optarg, ",", true);
+	for(vector<string>::iterator it = tmp.begin(); it != tmp.end(); ++it)
+	  fieldNums.push_back(strtoul(it->c_str(), NULL, 0));
+      }
       break;
 
     default:
@@ -74,7 +85,9 @@ main(int argc, char* argv[]) try
   // check args
   argc -= optind;
   const char* file(argv[optind++]);
-  if(argc != 1 || !fields.size())
+  if(argc != 1
+  || (fieldNames.size() && fieldNums.size())
+  || (!fieldNames.size() && !fieldNums.size()))
   {
     help(argv);
     return EXIT_FAILURE;
@@ -93,30 +106,48 @@ main(int argc, char* argv[]) try
   char* addr;
   char_matrix& m = *mapCharMatrix(fd, &addr, file, sep);
 
-  // build the column map
-  col_map cmap;
-  for(size_t i = 0; i != m.front().size(); ++i)
-  {
-    const char* cname = m.front()[i];
-    if(!cmap.insert(make_pair(string(cname), i)).second)
-    {
-      cerr << file << ": duplicated column \"" << cname << "\"\n";
-      return EXIT_FAILURE;
-    }
-  }
-
   // build the resulting column list
   vector<size_t> cols;
-  for(vector<string>::const_iterator it = fields.begin();
-      it != fields.end(); ++it)
+  if(fieldNums.size())
   {
-    col_map::const_iterator cIt = cmap.find(*it);
-    if(cIt == cmap.end())
+    // from column numbers
+    for(vector<size_t>::const_iterator it = fieldNums.begin();
+	it != fieldNums.end(); ++it)
     {
-      cerr << file << ": unknown column \"" << *it << "\"\n";
-      return EXIT_FAILURE;
+      if(!*it || *it > m.front().size())
+      {
+	cerr << file << ": invalid column number " << *it << "\n";
+	return EXIT_FAILURE;
+      }
+
+      cols.push_back(*it - 1);
     }
-    cols.push_back(cIt->second);
+  }
+  else
+  {
+    // from column names
+    col_map cmap;
+    for(size_t i = 0; i != m.front().size(); ++i)
+    {
+      const char* cname = m.front()[i];
+      if(!cmap.insert(make_pair(string(cname), i)).second)
+      {
+	cerr << file << ": duplicated column \"" << cname << "\"\n";
+	return EXIT_FAILURE;
+      }
+    }
+
+    for(vector<string>::const_iterator it = fieldNames.begin();
+	it != fieldNames.end(); ++it)
+    {
+      col_map::const_iterator cIt = cmap.find(*it);
+      if(cIt == cmap.end())
+      {
+	cerr << file << ": unknown column \"" << *it << "\"\n";
+	return EXIT_FAILURE;
+      }
+      cols.push_back(cIt->second);
+    }
   }
 
   // output
