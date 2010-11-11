0b1cc8b4d6e223ca5f64c85040ec649c97c495fb
Yuri D'Elia <yuri.delia@eurac.edu>
2010-11-11 12:31:18 +0100

    Support duplicated fields in tblcut. Implement -c (complement selection).

--- tblcut.cc
+++ tblcut.cc
@@ -12,8 +12,11 @@
 
 // system headers
 #include <map>
-using std::map;
+using std::multimap;
 using std::make_pair;
+
+#include <algorithm>
+using std::find;
 
 // c headers
 #include <stdlib.h>
@@ -23,7 +26,7 @@ using std::make_pair;
  * Types and constants
  */
 
-typedef map<string, size_t> col_map;
+typedef multimap<string, size_t> col_map;
 
 
 /*
@@ -42,6 +45,7 @@ help(char* argv[])
        << "  -d sep:		set a different column separator\n"
        << "  -f col,col,...:	extract the selected column names\n"
        << "  -n col,col,...:	extract the selected column numbers\n"
+       << "  -c:			complement the selected fields\n"
        << "  -h:			help summary\n";
 }
 
@@ -51,10 +55,11 @@ main(int argc, char* argv[]) try
 {
   vector<string> fieldNames;
   vector<size_t> fieldNums;
+  bool complement = false;
   char sep = 0;
 
   int arg;
-  while((arg = getopt(argc, argv, "hf:n:d:")) != -1)
+  while((arg = getopt(argc, argv, "hf:n:d:c")) != -1)
     switch(arg)
     {
     case 'h':
@@ -76,6 +81,10 @@ main(int argc, char* argv[]) try
 	for(vector<string>::iterator it = tmp.begin(); it != tmp.end(); ++it)
 	  fieldNums.push_back(strtoul(it->c_str(), NULL, 0));
       }
+      break;
+
+    case 'c':
+      complement = !complement;
       break;
 
     default:
@@ -110,7 +119,7 @@ main(int argc, char* argv[]) try
   vector<size_t> cols;
   if(fieldNums.size())
   {
-    // from column numbers
+    // check arguments
     for(vector<size_t>::const_iterator it = fieldNums.begin();
 	it != fieldNums.end(); ++it)
     {
@@ -119,34 +128,59 @@ main(int argc, char* argv[]) try
 	cerr << file << ": invalid column number " << *it << "\n";
 	return EXIT_FAILURE;
       }
+    }
 
-      cols.push_back(*it - 1);
+    // from column numbers
+    if(!complement)
+    {
+      for(vector<size_t>::const_iterator it = fieldNums.begin();
+	  it != fieldNums.end(); ++it)
+	cols.push_back(*it - 1);
+    }
+    else
+    {
+      for(size_t i = 0; i != m.front().size(); ++i)
+      {
+	if(find(fieldNums.begin(), fieldNums.end(), i + 1) == fieldNums.end())
+	  cols.push_back(i);
+      }
     }
   }
   else
   {
     // from column names
-    col_map cmap;
-    for(size_t i = 0; i != m.front().size(); ++i)
+    if(!complement)
     {
-      const string cname = m.front()[i];
-      if(!cmap.insert(make_pair(cname, i)).second)
+      col_map cmap;
+      for(size_t i = 0; i != m.front().size(); ++i)
       {
-	cerr << file << ": duplicated column \"" << cname << "\"\n";
-	return EXIT_FAILURE;
+	const string cname = m.front()[i];
+	cmap.insert(make_pair(string(m.front()[i]), i));
+      }
+
+      for(vector<string>::const_iterator it = fieldNames.begin();
+	  it != fieldNames.end(); ++it)
+      {
+	col_map::const_iterator cIt = cmap.lower_bound(*it);
+	if(cIt == cmap.end())
+	{
+	  cerr << file << ": unknown column \"" << *it << "\"\n";
+	  return EXIT_FAILURE;
+	}
+
+	for(col_map::const_iterator cEnd = cmap.upper_bound(*it);
+	    cIt != cEnd; ++cIt)
+	  cols.push_back(cIt->second);
       }
     }
-
-    for(vector<string>::const_iterator it = fieldNames.begin();
-	it != fieldNames.end(); ++it)
+    else
     {
-      col_map::const_iterator cIt = cmap.find(*it);
-      if(cIt == cmap.end())
+      for(vector<fix_string>::const_iterator it = m.front().begin();
+	  it != m.front().end(); ++it)
       {
-	cerr << file << ": unknown column \"" << *it << "\"\n";
-	return EXIT_FAILURE;
+	if(find(fieldNames.begin(), fieldNames.end(), string(*it)) == fieldNames.end())
+	  cols.push_back(it - m.front().begin());
       }
-      cols.push_back(cIt->second);
     }
   }
 
